261bbc4b9e5e0a3fefaa578400d5c4e1cd0354dc
poornendhupkumar-afk <poornendhupkumar@gmail.com>
2026-06-17 23:51:33 +0530

    Professional portfolio upgrade

--- script.js
+++ script.js
@@ -56,3 +56,29 @@ View Project
 });
 
 });
+const images =
+document.querySelectorAll(".certificate-image");
+
+const modal =
+document.getElementById("modal");
+
+const modalImg =
+document.getElementById("modal-img");
+
+images.forEach(img=>{
+
+img.addEventListener("click",()=>{
+
+modal.style.display="flex";
+
+modalImg.src = img.src;
+
+});
+
+});
+
+modal.addEventListener("click",()=>{
+
+modal.style.display="none";
+
+});
